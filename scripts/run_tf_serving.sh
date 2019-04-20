52cc56a98f1a0a1dbb7b80aae6dc3a4ff85af49f
lain-m21 <666fullbloom@gmail.com>
2019-04-20 22:44:47 +0900

    gRPC and REST port forward option

--- scripts/run_tf_serving.sh
+++ scripts/run_tf_serving.sh
@@ -3,9 +3,11 @@
 TF_SERVING_IMAGE="tensorflow/serving:latest"
 SAVED_MODEL_PATH=$1
 SERVING_MODEL_NAME=$2
-NUM_THREADS=$3
+GRPC_PORT_TUNNEL=$3
+REST_PORT_TUNNEL=$4
+NUM_THREADS=$5
 
-docker run -d -p 9000:8500 \
+docker run -d -p ${GRPC_PORT_TUNNEL}:8500 -p ${REST_PORT_TUNNEL}:8501 \
     -v $(pwd)/data/${SAVED_MODEL_PATH}:/models/${SAVED_MODEL_PATH} -e MODEL_NAME=${SERVING_MODEL_NAME} \
     -t ${TF_SERVING_IMAGE} \
     --tensorflow_intra_op_parallelism=${NUM_THREADS} \
