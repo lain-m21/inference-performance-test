97be97eda405b26c7644ae551b28cf6bb70b1914
Lain Matsuoka <lain21@mercari.com>
2019-04-24 16:33:56 +0900

    removed unnecessary threading options

--- scripts/run_tf_serving_optimized.sh
+++ scripts/run_tf_serving_optimized.sh
@@ -4,10 +4,7 @@ TF_SERVING_IMAGE="tmp/tensorflow-serving-devel:0.0.1"
 SAVED_MODEL_NAME=$1
 GRPC_PORT_TUNNEL=$2
 REST_PORT_TUNNEL=$3
-NUM_THREADS=$4
 
 docker run -d -p ${GRPC_PORT_TUNNEL}:8500 -p ${REST_PORT_TUNNEL}:8501 \
     -v $(pwd)/data/${SAVED_MODEL_NAME}:/models/${SAVED_MODEL_NAME} -e MODEL_NAME=${SAVED_MODEL_NAME} \
-    -t ${TF_SERVING_IMAGE} \
-    --tensorflow_intra_op_parallelism=${NUM_THREADS} \
-    --tensorflow_inter_op_parallelism=${NUM_THREADS}
+    -t ${TF_SERVING_IMAGE}
