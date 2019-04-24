c691d303c98987922861b8fc6d9fc6e9440b3e94
Lain Matsuoka <lain21@mercari.com>
2019-04-24 18:11:51 +0900

    japronto http client server

---
+++ src/japronto_app.py
@@ -0,0 +1,53 @@
+import json
+import logging
+import argparse
+from japronto import Application
+
+from src.api import PredictionService
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+
+app = Application()
+router = app.router
+api = PredictionService()
+
+
+def predict(request):
+    data = json.loads(request.json)
+    input_data = data['input']
+    outputs = api.predict(input_data)
+    return request.Response(text=json.dumps({'outputs': outputs.tolist()}))
+
+
+def health(request):
+    logger.info("[HEALTH] GET")
+    return request.Response(text='')
+
+
+router.add_route('/', health, method='GET')
+router.add_route('/predict', predict, method='POST')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host',
+                        default='0.0.0.0',
+                        type=str,
+                        help='host of predictor')
+    parser.add_argument('--port',
+                        default=8501,
+                        type=int,
+                        help='port of predictor')
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='debug mode')
+    args = parser.parse_args()
+
+    app.run(host=args.host, port=args.port, debug=args.debug)
+
+
+if __name__ == '__main__':
+    main()
