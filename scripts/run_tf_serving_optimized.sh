aaff23f057b8698b91eaa78377915167412a3c3a
Lain Matsuoka <lain21@mercari.com>
2019-04-24 13:53:29 +0900

    bug fixed - use same saved_model_path for model_name

--- scripts/run_tf_serving_optimized.sh
+++ scripts/run_tf_serving_optimized.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 TF_SERVING_IMAGE="tmp/tensorflow-serving-devel:0.0.1"
-SAVED_MODEL_PATH=$1
-SERVING_MODEL_NAME=$2
-GRPC_PORT_TUNNEL=$3
-REST_PORT_TUNNEL=$4
-NUM_THREADS=$5
+SAVED_MODEL_NAME=$1
+GRPC_PORT_TUNNEL=$2
+REST_PORT_TUNNEL=$3
+NUM_THREADS=$4
 
 docker run -d -p ${GRPC_PORT_TUNNEL}:8500 -p ${REST_PORT_TUNNEL}:8501 \
-    -v $(pwd)/data/${SAVED_MODEL_PATH}:/models/${SAVED_MODEL_PATH} -e MODEL_NAME=${SERVING_MODEL_NAME} \
+    -v $(pwd)/data/${SAVED_MODEL_NAME}:/models/${SAVED_MODEL_NAME} -e MODEL_NAME=${SAVED_MODEL_NAME} \
     -t ${TF_SERVING_IMAGE} \
     --tensorflow_intra_op_parallelism=${NUM_THREADS} \
     --tensorflow_inter_op_parallelism=${NUM_THREADS}
