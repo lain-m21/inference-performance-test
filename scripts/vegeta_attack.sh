c6f31d1aab9f70b51788974a6b7b609c0413954e
lain-m21 <666fullbloom@gmail.com>
2019-04-29 14:05:22 +0900

    output file argument

--- scripts/vegeta_attack.sh
+++ scripts/vegeta_attack.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 RATE=$1
 DURATION=$2
+if [[ $# = 3 ]]; then
+  OUTPUT=$3
+else
+  OUTPUT=tools/vegeta.bin
+fi
 
-vegeta attack -rate=${RATE} -duration=${DURATION}s -targets=tools/target.txt > tools/vegeta.bin
+vegeta attack -rate=${RATE} -duration=${DURATION}s -targets=tools/target.txt > ${OUTPUT}
 cat vegeta.bin | vegeta report
 cat vegeta.bin | vegeta report -type='hist[0,100ms,200ms,300ms,400ms,500ms,1s,2s,3s,4s,5s]'
