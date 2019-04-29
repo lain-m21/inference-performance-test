d699a849c476f33de360d0efbaa3c74c85a08cc0
lain-m21 <666fullbloom@gmail.com>
2019-04-29 14:18:11 +0900

    optional arguments for target file and output bin

--- scripts/vegeta_attack.sh
+++ scripts/vegeta_attack.sh
@@ -2,12 +2,11 @@
 
 RATE=$1
 DURATION=$2
-if [[ $# = 3 ]]; then
-  OUTPUT=$3
-else
-  OUTPUT=tools/vegeta.bin
-fi
+TARGET=${3:-tools/target.txt}
+OUTPUT=${4:-tools/vegeta_result.bin}
 
-vegeta attack -rate=${RATE} -duration=${DURATION}s -targets=tools/target.txt > ${OUTPUT}
+echo "Vegeta attack - rate = ${RATE}, duration = ${DURATION}, target = ${TARGET}, output = ${OUTPUT}"
+
+vegeta attack -rate=${RATE} -duration=${DURATION}s -targets=${TARGET} > ${OUTPUT}
 cat vegeta.bin | vegeta report
 cat vegeta.bin | vegeta report -type='hist[0,100ms,200ms,300ms,400ms,500ms,1s,2s,3s,4s,5s]'
