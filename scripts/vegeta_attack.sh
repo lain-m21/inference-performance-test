d5faaa9f964adec8c00b0d0293e9ec318c939680
Lain Matsuoka <lain21@mercari.com>
2019-05-01 14:04:48 +0900

    bug fixed

--- scripts/vegeta_attack.sh
+++ scripts/vegeta_attack.sh
@@ -8,5 +8,5 @@ OUTPUT=${4:-tools/vegeta_result.bin}
 echo "Vegeta attack - rate = ${RATE}, duration = ${DURATION}, target = ${TARGET}, output = ${OUTPUT}"
 
 vegeta attack -rate=${RATE} -duration=${DURATION}s -targets=${TARGET} > ${OUTPUT}
-cat vegeta.bin | vegeta report
-cat vegeta.bin | vegeta report -type='hist[0,100ms,200ms,300ms,400ms,500ms,1s,2s,3s,4s,5s]'
+cat ${OUTPUT} | vegeta report
+cat ${OUTPUT} | vegeta report -type='hist[0,100ms,200ms,300ms,400ms,500ms,1s,2s,3s,4s,5s]'
