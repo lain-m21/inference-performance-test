d84c61f5af0b3d83ec96e7564cb2becf588f24fa
Lain Matsuoka <lain21@mercari.com>
2019-05-01 13:49:17 +0900

    sanic

---
+++ src/sanic_app.py
@@ -0,0 +1,51 @@
+import json
+import logging
+import argparse
+from sanic import Sanic
+from sanic.response import json
+
+from src.api import PredictionService
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+
+app = Sanic()
+api = PredictionService()
+
+
+@app.route('/predict', methods=['POST'])
+async def predict(request):
+    data = json.loads(request.json)
+    input_data = data['input']
+    outputs = api.predict(input_data)
+    return json({'outputs': outputs.tolist()})
+
+
+@app.route('/', methods=['GET'])
+def health():
+    logger.info("[HEALTH] GET")
+    return ''
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host',
+                        default='0.0.0.0',
+                        type=str,
+                        help='host of predictor')
+    parser.add_argument('--port',
+                        default=8501,
+                        type=int,
+                        help='port of predictor')
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='debug mode')
+    args = parser.parse_args()
+
+    app.run(host=args.host, port=args.port, debug=args.debug)
+
+
+if __name__ == '__main__':
+    main()
