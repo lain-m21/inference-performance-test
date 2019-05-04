babbaace743bcc526baf3932b4c6cc39e36999e1
lain-m21 <666fullbloom@gmail.com>
2019-05-04 23:35:49 +0900

    example usage

--- scripts/vegeta_attack.sh
+++ scripts/vegeta_attack.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Ex: ./scripts/vegeta_attack.sh 10 5 ./tools/target_onnx.txt ./data/vegeta_result_onnx.bin
+
 RATE=$1
 DURATION=$2
 TARGET=${3:-tools/target.txt}
-OUTPUT=${4:-tools/vegeta_result.bin}
+OUTPUT=${4:-data/vegeta_result.bin}
 
 echo "Vegeta attack - rate = ${RATE}, duration = ${DURATION}, target = ${TARGET}, output = ${OUTPUT}"
 
