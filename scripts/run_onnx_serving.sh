7b5da0e04803db34da90c0a0fae56d8e0ccf91c1
Lain Matsuoka <lain21@mercari.com>
2019-05-07 11:26:35 +0900

    container delete option

--- scripts/run_onnx_serving.sh
+++ scripts/run_onnx_serving.sh
@@ -7,9 +7,12 @@ SERVABLE_TYPE=$2
 MODEL_INFO_PATH=$3
 REST_PORT_TUNNEL=$4
 
-ONNX_SERVING_IMAGE="tmp/onnx-serving-${FRAMEWORK}:0.0.1"
+ONNX_SERVING_IMAGE="tmp/onnx-serving:0.0.1"
 DATA_DIR="/workspace/data"
 
-docker run -d -p ${REST_PORT_TUNNEL}:8501 \
-    -v $(pwd)/data:${DATA_DIR} -e MODEL_INFO_PATH=${DATA_DIR}/${MODEL_INFO_PATH} -e SERVABLE_TYPE=${SERVABLE_TYPE} \
+docker run -d -p ${REST_PORT_TUNNEL}:8501 --rm \
+    -v $(pwd)/data:${DATA_DIR} \
+    -e MODEL_INFO_PATH=${DATA_DIR}/${MODEL_INFO_PATH} \
+    -e SERVABLE_TYPE=${SERVABLE_TYPE} \
+    -e FRAMEWORK=${FRAMEWORK} \
     -t ${ONNX_SERVING_IMAGE}
