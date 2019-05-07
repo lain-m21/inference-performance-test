eb28594f38657c94e84d8051ac0746db11a71681
Lain Matsuoka <lain21@mercari.com>
2019-05-07 14:48:16 +0900

    container name

--- scripts/run_onnx_serving.sh
+++ scripts/run_onnx_serving.sh
@@ -6,11 +6,12 @@ FRAMEWORK=$1
 SERVABLE_TYPE=$2
 MODEL_INFO_PATH=$3
 REST_PORT_TUNNEL=$4
+CONTAINER_NAME=${5:-onnx_serving}
 
 ONNX_SERVING_IMAGE="tmp/onnx-serving:0.0.1"
 DATA_DIR="/workspace/data"
 
-docker run -d -p ${REST_PORT_TUNNEL}:8501 --rm \
+docker run -d -p ${REST_PORT_TUNNEL}:8501 --rm -n ${CONTAINER_NAME}\
     -v $(pwd)/data:${DATA_DIR} \
     -e MODEL_INFO_PATH=${DATA_DIR}/${MODEL_INFO_PATH} \
     -e SERVABLE_TYPE=${SERVABLE_TYPE} \
--- scripts/run_tf_serving_optimized.sh
+++ scripts/run_tf_serving_optimized.sh
@@ -6,7 +6,8 @@ TF_SERVING_IMAGE="tmp/tensorflow-serving-devel:0.0.1"
 SAVED_MODEL_NAME=$1
 GRPC_PORT_TUNNEL=$2
 REST_PORT_TUNNEL=$3
+CONTAINER_NAME=${4:-tf_serving}
 
-docker run -d -p ${GRPC_PORT_TUNNEL}:8500 -p ${REST_PORT_TUNNEL}:8501 --rm \
+docker run -d -p ${GRPC_PORT_TUNNEL}:8500 -p ${REST_PORT_TUNNEL}:8501 --rm -n ${CONTAINER_NAME} \
     -v $(pwd)/data/${SAVED_MODEL_NAME}:/models/${SAVED_MODEL_NAME} -e MODEL_NAME=${SAVED_MODEL_NAME} \
     -t ${TF_SERVING_IMAGE}
