ac5d7381388c756c78ba5086c5f6070a71e36596
lain-m21 <666fullbloom@gmail.com>
2019-05-19 16:33:16 +0900

    NUM_HTTP_THREADS is specified as container argument

--- scripts/run_onnxruntime_serving.sh
+++ scripts/run_onnxruntime_serving.sh
@@ -6,8 +6,10 @@ ONNXRUNTIME_SERVING_IMAGE="tmp/onnxruntime-serving:0.0.1"
 
 SAVED_MODEL_NAME=$1
 REST_PORT_TUNNEL=$2
-CONTAINER_NAME=${3:-onnxruntime_serving}
+NUM_HTTP_THREADS=${3:-$(nproc)}
+CONTAINER_NAME=${4:-onnxruntime_serving}
 
 docker run -d -p ${REST_PORT_TUNNEL}:8001 --rm --name ${CONTAINER_NAME} \
-    -v $(pwd)/data/${SAVED_MODEL_NAME}:/models/${SAVED_MODEL_NAME} -e MODEL_NAME=${SAVED_MODEL_NAME} \
+    -v $(pwd)/data/${SAVED_MODEL_NAME}:/models/${SAVED_MODEL_NAME} \
+    -e MODEL_NAME=${SAVED_MODEL_NAME} -e NUM_HTTP_THREADS=${NUM_HTTP_THREADS}\
     -t ${ONNXRUNTIME_SERVING_IMAGE}
