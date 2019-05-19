612baeb6ad7b6657ae0d4e92c1d38d5951e7f008
lain-m21 <666fullbloom@gmail.com>
2019-05-19 16:20:32 +0900

    onnxruntime server launch script

--- scripts/run_onnxruntime_serving.sh
+++ scripts/run_onnxruntime_serving.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Ex: ./scripts/run_tf_serving_optimized.sh densenet121_tf 8500 8501
+# Ex: ./scripts/run_onnxruntime_serving_optimized.sh densenet121 8501
 
 ONNXRUNTIME_SERVING_IMAGE="tmp/onnxruntime-serving:0.0.1"
+
 SAVED_MODEL_NAME=$1
-GRPC_PORT_TUNNEL=$2
-REST_PORT_TUNNEL=$3
-CONTAINER_NAME=${4:-onnxruntime_serving}
+REST_PORT_TUNNEL=$2
+CONTAINER_NAME=${3:-onnxruntime_serving}
 
 docker run -d -p ${REST_PORT_TUNNEL}:8001 --rm --name ${CONTAINER_NAME} \
     -v $(pwd)/data/${SAVED_MODEL_NAME}:/models/${SAVED_MODEL_NAME} -e MODEL_NAME=${SAVED_MODEL_NAME} \
