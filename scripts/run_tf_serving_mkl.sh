2d9978728b81e3fe8dbf330b6021a7e307e6fed5
lain-m21 <666fullbloom@gmail.com>
2019-05-21 12:41:48 +0900

    MKL-supported tensorflow serving

---
+++ scripts/run_tf_serving_mkl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ex: ./scripts/run_tf_serving_optimized.sh densenet121_tf 8500 8501
+
+TF_SERVING_IMAGE="tmp/tensorflow-serving-mkl:0.0.1"
+SAVED_MODEL_NAME=$1
+GRPC_PORT_TUNNEL=$2
+REST_PORT_TUNNEL=$3
+CONTAINER_NAME=${4:-tf_serving}
+
+docker run -d -p ${GRPC_PORT_TUNNEL}:8500 -p ${REST_PORT_TUNNEL}:8501 --rm --name ${CONTAINER_NAME} \
+    -v $(pwd)/data/${SAVED_MODEL_NAME}:/models/${SAVED_MODEL_NAME} -e MODEL_NAME=${SAVED_MODEL_NAME} \
+    -t ${TF_SERVING_IMAGE}
