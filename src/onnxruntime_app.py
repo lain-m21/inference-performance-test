a513fef8742f5fe9a5701022a3fcb25334b288c6
lain-m21 <666fullbloom@gmail.com>
2019-05-21 14:58:40 +0900

    frontend interfaces

--- src/onnxruntime_app.py
+++ src/onnxruntime_app.py
@@ -1,23 +1,37 @@
 import os
+import logging
 import importlib
 import requests
 from sanic import Sanic
-import numpy as np
-import onnxruntime
+from sanic.response import json as sanic_json
 
-from src.utils import onnx_predict_pb2, onnx_ml_pb2
+
+logger = logging.getLogger(__name__)
 
 
 app = Sanic()
+target = os.environ.get('TARGET', 'template')
+endpoint = os.environ.get('ENDPOINT', 'http://localhost:9000/v1/models/mymodel/versions/1:predict')
 
+request_headers = {
+    'Content-Type': 'application/octet-stream',
+    'Accept': 'application/octet-stream'
+}
+
+input_module_name = 'src.input_preparation.onnxruntime.{}'.format(target)
+output_module_name = 'src.output_decoding.onnxruntime.{}'.format(target)
+input_module = importlib.import_module(input_module_name)
+output_module = importlib.import_module(output_module_name)
 
 
 @app.route('/predict', methods=['POST'])
 async def predict(request):
-    data = request.json
-    input_data = data['input']
-    outputs = api.predict(input_data)
-    return sanic_json({'outputs': outputs.tolist()})
+    predict_request = input_module.prepare_request(request)
+    payload = predict_request.SerializeToString()
+
+    res = requests.post(endpoint, headers=request_headers, data=payload)
+    outputs = output_module.decode_response(res)
+    return sanic_json({'outputs': outputs})
 
 
 @app.route('/', methods=['GET'])
---
+++ src/tfserving_app.py
@@ -0,0 +1,38 @@
+import os
+import logging
+import importlib
+import grpc
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+from sanic import Sanic
+from sanic.response import json as sanic_json
+
+
+logger = logging.getLogger(__name__)
+
+
+app = Sanic()
+target = os.environ.get('TARGET', 'template')
+endpoint = os.environ.get('ENDPOINT', 'localhost:9000')
+
+channel = grpc.insecure_channel(endpoint)
+stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+
+input_module_name = 'src.input_preparation.tensorflow.{}'.format(target)
+output_module_name = 'src.output_decoding.tensorflow.{}'.format(target)
+input_module = importlib.import_module(input_module_name)
+output_module = importlib.import_module(output_module_name)
+
+
+@app.route('/predict', methods=['POST'])
+async def predict(request):
+    predict_request = input_module.prepare_request(request)
+
+    res = stub.Predict(predict_request, 30)
+    outputs = output_module.decode_response(res)
+    return sanic_json({'outputs': outputs})
+
+
+@app.route('/', methods=['GET'])
+async def health(request):
+    logger.info("[HEALTH] GET")
+    return sanic_json({'status': 'OK'})
